e282dca3adf2910a62653f64fd0c9f252ae391cc
SaidaAnge <saidaangenakeu@gmail.com>
2025-07-21 20:14:10 +0200

    ci: modification mission/urls.py

--- mission_manager/urls.py
+++ mission_manager/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='dashboard', permanent=False)),
     path('', include('core.urls')),  # Inclut les URLs de l'app core
 ]
 
