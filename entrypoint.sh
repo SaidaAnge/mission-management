c3c2f008002912860eeec099564fd8ace70cd193
SaidaAnge <saidaangenakeu@gmail.com>
2025-07-21 20:47:59 +0200

    ci: add update syntax for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,15 @@ set -e
 # 1) Run migrations
 python manage.py migrate --noinput
 
-# 2) Create superuser if not exists
-python - <<'EOF'
+# 2) Create superuser if not exists via manage.py shell
+python manage.py shell <<'EOF'
 from django.contrib.auth import get_user_model
 import os
 
 User = get_user_model()
-username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'saida')
-email    = os.getenv('DJANGO_SUPERUSER_EMAIL',    'saidaangenakeu@gmail.com')
-password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'Qwerty@123')
+username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+email    = os.getenv('DJANGO_SUPERUSER_EMAIL',    'admin@example.com')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpass')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
